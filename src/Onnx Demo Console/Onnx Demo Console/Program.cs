df754a0008fef0942590dfabad11ace43847a496
volody <volody@avisosoft.com>
2020-03-15 14:43:28 -0400

    added ResizeImage

--- src/Onnx Demo Console/Onnx Demo Console/Program.cs
+++ src/Onnx Demo Console/Onnx Demo Console/Program.cs
@@ -2,7 +2,9 @@
 using Microsoft.ML.OnnxRuntime.Tensors;
 using System;
 using System.Collections.Generic;
-using System.IO;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Drawing.Imaging;
 using System.Linq;
 
 namespace Onnx_Demo_Console
@@ -21,21 +23,26 @@ namespace Onnx_Demo_Console
         static void Main(string[] args)
         {
             var session = new InferenceSession(@"model\model.onnx");
-            var img = ConvertImageToByteArray(@"input\sample_image.png");
-            var result = PredictLocal(session, img);
+            var img = Image.FromFile(@"input\sample_image.png");
+            var bmp = ResizeImage(img, 28, 28);
+            var data = ConvertImageToByteArray(bmp);
+            var result = PredictLocal(session, data);
             Console.WriteLine(result);
         }
 
-        public static float[] ConvertImageToByteArray(string imagePath)
+        public static float[] ConvertImageToByteArray(Bitmap image)
         {
-            float[] imageByteArray = null;
-            FileStream fileStream = new FileStream(imagePath, FileMode.Open, FileAccess.Read);
-            using (BinaryReader reader = new BinaryReader(fileStream))
+            var imageByteArray = new float[image.Width * image.Height] ;
+
+            for (int x = 0; x < image.Width; x++)
             {
-                imageByteArray = new float[reader.BaseStream.Length];
-                for (int i = 0; i < reader.BaseStream.Length; i++)
-                    imageByteArray[i] = reader.ReadByte();
+                for (int y = 0; y < image.Height; y++)
+                {
+                    var p = image.GetPixel(x, y);
+                    imageByteArray[y * image.Width + x] = 255 - (((float)(p.R) + p.G + p.B) / 3);
+                }
             }
+
             return imageByteArray;
         }
 
@@ -73,5 +80,36 @@ namespace Onnx_Demo_Console
             }
         }
 
+        /// <summary>
+        /// Resize the image to the specified width and height.
+        /// </summary>
+        /// <param name="image">The image to resize.</param>
+        /// <param name="width">The width to resize to.</param>
+        /// <param name="height">The height to resize to.</param>
+        /// <returns>The resized image.</returns>
+        public static Bitmap ResizeImage(Image image, int width, int height)
+        {
+            var destRect = new Rectangle(0, 0, width, height);
+            var destImage = new Bitmap(width, height);
+
+            destImage.SetResolution(image.HorizontalResolution, image.VerticalResolution);
+
+            using (var graphics = Graphics.FromImage(destImage))
+            {
+                graphics.CompositingMode = CompositingMode.SourceCopy;
+                graphics.CompositingQuality = CompositingQuality.HighQuality;
+                graphics.InterpolationMode = InterpolationMode.HighQualityBicubic;
+                graphics.SmoothingMode = SmoothingMode.HighQuality;
+                graphics.PixelOffsetMode = PixelOffsetMode.HighQuality;
+
+                using (var wrapMode = new ImageAttributes())
+                {
+                    wrapMode.SetWrapMode(WrapMode.TileFlipXY);
+                    graphics.DrawImage(image, destRect, 0, 0, image.Width, image.Height, GraphicsUnit.Pixel, wrapMode);
+                }
+            }
+
+            return destImage;
+        }
     }
 }
